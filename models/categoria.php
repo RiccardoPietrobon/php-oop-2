1fafac1c8c3eed0c3a35edd16c3bd38052fa7898
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-03-25 12:36:17 +0100

    estengo nuove classi

--- models/categoria.php
+++ models/categoria.php
@@ -3,20 +3,34 @@
 class Categoria
 {
     protected $nome;
+    protected $icona;
 
-    public function __construct(string $nome)
+
+    public function __construct(string $nome, string $icona)
     {
         $this->setNome($nome);
+        $this->setIcona($icona);
     }
 
     public function setNome($nome)
     {
-        if ($nome !== "Gatto" || $nome !== "Cane") return false; //parametri stringa
+        if ($nome !== "Gatto" || $nome !== "Cane") return false; //parametri stringa Cane o gatto
         $this->nome = $nome;
     }
 
     public function getNome()
     {
         return $this->nome;
+    }
+    //********************************************************************** */
+    public function setIcona($icona)
+    {
+        if ($icona !== "icona_gatto" || $icona !== "icona_cane") return false; //parametri stringa Cane o gatto icona
+        $this->icona = $icona;
+    }
+
+    public function getIcona()
+    {
+        return $this->icona;
     }
 }
